8c72cb22ad792eeb4e26c3109b25e280faac91de
jente_tav <54480755+jente01t@users.noreply.github.com>
2024-12-04 09:45:59 +0100

    model update

--- train.py
+++ train.py
@@ -8,38 +8,40 @@ from sklearn.preprocessing import LabelEncoder
 from tensorflow.python.keras.utils.np_utils import to_categorical
 import tensorflow as tf; tf.keras
 
-# Load data and labels
+
+
+from keras import __version__
+tf.keras.__version__ = __version__
+
 dir = './data'
 data = np.load(os.path.join(dir, 'data.npy'))
 labels = np.load(os.path.join(dir, 'labels.npy'))
 
-# Encode labels
 label_encoder = LabelEncoder()
 labels = label_encoder.fit_transform(labels)
-labels = to_categorical(labels)  # Convert to one-hot encoding
+labels = to_categorical(labels)  
 
-# Split data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, random_state=42)
 
-# Define the model
-model = Sequential()
-model.add(LSTMV1(64, return_sequences=True, activation='relu', input_shape=(X_train.shape[1], X_train.shape[2])))
-model.add(LSTMV1(64, return_sequences=False, activation='relu'))
-model.add(Dense(64, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(labels.shape[1], activation='softmax'))
 
-# Compile the model
+with tf.device('/cpu:0'):
+    model = Sequential()
+    model.add(LSTMV1(64, return_sequences=True, activation='relu', input_shape=(X_train.shape[1], X_train.shape[2])))
+    model.add(LSTMV1(64, return_sequences=False, activation='relu'))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(labels.shape[1], activation='softmax'))
+
+
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
-# Train the model
+
 history = model.fit(X_train, y_train, epochs=30, batch_size=32, validation_data=(X_test, y_test))
 
-# Evaluate the model
+
 loss, accuracy = model.evaluate(X_test, y_test)
 print(f'Test Accuracy: {accuracy * 100:.2f}%')
 
-# Save the model
+
 model.save(os.path.join(dir, 'model.h5'))
 print('Model saved to disk.')
-
