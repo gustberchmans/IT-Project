6fc0d749578ec330bde111ca7e77cd5fa447f27d
jente_tav <54480755+jente01t@users.noreply.github.com>
2024-12-04 09:48:00 +0100

    Use cpu for model training

--- data_collection.py
+++ data_collection.py
@@ -3,7 +3,7 @@ import cv2
 import mediapipe as mp
 import numpy as np
 
-# Create directory if it doesn't exist
+
 dir = './data'
 if not os.path.exists(dir):
     try:
@@ -14,7 +14,7 @@ if not os.path.exists(dir):
 else:
     print(f'Directory already exists: {dir}')
 
-# Initialize video capture
+
 videoCapture = cv2.VideoCapture(0)
 if not videoCapture.isOpened():
     print('Error: Could not access the webcam.')
@@ -23,11 +23,11 @@ else:
     print('Webcam accessed successfully.')
 
 
-# Parameters
+
 sets = 3
 frames_per_sequence = 30
 
-# Collect data
+
 for j in range(sets):
     class_dir = os.path.join(dir, str(j))
     if not os.path.exists(class_dir):
@@ -55,10 +55,10 @@ for j in range(sets):
             if cv2.waitKey(25) & 0xFF == ord('q'):
                 break
 
-            # Append the frame to the sequence
+            
             frames.append(frame)
 
-        # Save the sequence as a numpy array
+  
         np.save(os.path.join(class_dir, f'sequence_{sequence}.npy'), np.array(frames))
 
 videoCapture.release()
--- train.py
+++ train.py
@@ -24,13 +24,13 @@ labels = to_categorical(labels)
 X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, random_state=42)
 
 
-with tf.device('/cpu:0'):
-    model = Sequential()
-    model.add(LSTMV1(64, return_sequences=True, activation='relu', input_shape=(X_train.shape[1], X_train.shape[2])))
-    model.add(LSTMV1(64, return_sequences=False, activation='relu'))
-    model.add(Dense(64, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(labels.shape[1], activation='softmax'))
+
+model = Sequential()
+model.add(LSTMV1(64, return_sequences=True, activation='relu', input_shape=(X_train.shape[1], X_train.shape[2])))
+model.add(LSTMV1(64, return_sequences=False, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(labels.shape[1], activation='softmax'))
 
 
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
